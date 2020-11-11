5ac2d3a59ab5e81a1b4e6ece4057658c6f3347e7
Sergey Klimov <desire0417@gmail.com>
2020-11-11 21:05:21 +0300

    исправляет ошибку

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import {createApi} from '../services/api';
 import {applyMiddleware, createStore} from 'redux';
-import reducer from './root-reducer';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+
+import reducer from './root-reducer';
 
 const api = createApi();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/actions.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
